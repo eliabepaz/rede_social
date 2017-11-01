7e0aeb2d7bd4d95789832c971e0b2efa0900ae7f
eliabepaz <32855849+eliabepaz@users.noreply.github.com>
2017-11-01 10:30:00 -0200

    Update user.py

--- user.py
+++ user.py
@@ -1,26 +1,46 @@
 class User:
+
     def __init__(self):
-        self.nome = ""
-        self.endereco = ""
+        self.nome = str
+        self.endereco = str
         self.data_nsc = int
         self.telefone = int
+        self.email = str
+        self.senha = str
+
 
     def get_nome(self):
         return self.nome
-    def set_nome(self, nome):
-        self.nome = nome
+    def set_nome(self, novo_nome):
+        self.nome = novo_nome
 
     def get_endereco(self):
         return self.endereco
-    def set_endereco(self, endereco):
-        self.endereco = endereco
+    def set_endereco(self, novo_endereco):
+        self.endereco = novo_endereco
 
     def get_data_nsc(self):
         return self.data_nsc
-    def set_data_nsc(self, data_nsc):
-        self.data_nsc = data_nsc
+    def set_data_nsc(self, novo_data_nsc):
+        self.data_nsc = novo_data_nsc
 
     def get_telefone(self):
         return self.telefone
-    def set_telefone(self, telefone):
-        self.telefone = telefone
+    def set_telefone(self, novo_telefone):
+        self.telefone = novo_telefone
+
+    def set_email(self,novo_email):
+        self.email = novo_email
+    def set_senha(self, nova_senha):
+        self.senha = nova_senha
+
+    def logar(self):
+        resposta = str
+        while(resposta != 'loguin efetuado'):
+            e = input("Insira o email:")
+            s = input ("Senha:")
+            if e == self.email and s == self.senha:
+                print('Loguin efetuado' )
+            else:
+                print('email ou senha incorretos')
+                respoata = ('email ou senha incorretos')
