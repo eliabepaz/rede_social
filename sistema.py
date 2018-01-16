f1559630d4bf8d794769b97000a06a1ebe98ff0c
eliabepaz <32855849+eliabepaz@users.noreply.github.com>
2018-01-16 14:11:00 -0300

    Aperfeiçoamento
    Classes desnecessárias foram removidas, o menu está executando a maioria das funções, falta corrigir a atribuição a listas do email e senha.

--- sistema.py
+++ sistema.py
@@ -1,3 +1,10 @@
+from acounts import Loguin
+from BD import log
+from acounts import Dados
+conta = Dados
+log_dados = Loguin
+log_list = log
+
 class Sistem:
     def __init__(self):
         self.identificador = None
@@ -5,76 +12,23 @@ class Sistem:
     def menu(self):
         print('1 - Cadastrar conta')
         print('2 - Mostrar conta')
+        print('3 - Cadastrar Email e Senha')
         print('x - Sair')
-        opcao = input('Digite a opção:')
+        opcao = input('Digite a opção: ')
+
         if opcao == '1':
-            Dado.cadastro()
+            print('\n\n\n')
+            conta.nome = input('Digite seu nome: ')
+            conta.idade = input('Digite sua idade: ')
+            conta.telefone = input('Digite seu telefone: ')
+            conta.endereco = input('Digite seu endereço: ')
+            print('\n\n\n')
         if opcao == '2':
-            ID = input('Conta:')
-            Conts.exibir_dados(ID)
+            print('Nome do usuario: ' + str(conta.nome) + '\nIdade: ' + str(conta.idade) + '\nTelefone: ' + str(conta.telefone) + '\nEndereço: ' + str(conta.endereco))
+
+        if opcao == '3':
+            log_dados.email = input('Digite um email valido:')
+            log_dados.senha = input('Digite um senha:')
+            log_list.emails.append(log_dados.email)
+            log_list.senhas.append(log_dados.senha)
         return(opcao)
-
-class Dado:
-    def __init__(self, nome, endereco, data_nsc, telefone, email, senha):
-        self.nome = nome
-        self.endereco = endereco
-        self.data_nsc = data_nsc
-        self.telefone = telefone
-        self.email = email
-        self.senha = senha
-        self.defalt_dados = []
-        self.loguin_dados = []
-    def cadastro(self):
-        self.nome = input(str('Digite seu nome: '))
-        self.defalt_dados.append(self.nome)
-        self.endereco = input(str('Digite seu endereço: '))
-        self.defalt_dados.append(self.endereco)
-        self.data_nsc = input(str('Digite sua data de nascimento: '))
-        self.defalt_dados.append(self.data_nsc)
-        self.telefone = input(str('Digite seu telefone: '))
-        self.defalt_dados.append(self.telefone)
-        self.email = input(str('Digite um email valido: '))
-        self.loguin_dados.append(self.email)
-        self.senha = input(str('Digite uma senha: '))
-        self.loguin_dados.append(self.senha)
-        User.start(self.defalt_dados, self.loguin_dados)
-
-class User:
-    def __init__(self):
-        self.num_user = 0
-    def id(self):
-        self.num_user += 1
-    def start(self , l_defalt, l_loguin):
-        i_name = self.num_user
-        Sistem.identificador = i_name
-        Lista_Encadeada.inserir_dados(i_name, l_defalt, l_loguin)
-        self.id()
-
-
-
-class Lista_Encadeada:
-    def __init__(self):
-        self.inicio = None
-    def inserir_dados(self, name, dados, log):
-        novo_no = No(name)
-        novo_no.proximo = self.inicio
-        self.inicio = novo_no
-        self.inicio.info = dados
-        self.log = log
-        Conts.armazenar(self.inicio)
-
-class No:
-    def __init__(self, valor):
-        self.dado = valor
-        self.proximo = None
-        self.info = None
-
-class Conts:
-    def __init__(self):
-        self.contas = []
-    def armazenar(self, usuario):
-        self.contas.append(usuario)
-    def exibir_dados(self,id):
-        if id in self.contas:
-            for x in self.contas.id:
-                print(x)
