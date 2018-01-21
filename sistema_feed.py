7e1f54d20d10ece3472a779a5eb1f603a06207b9
eliabepaz <32855849+eliabepaz@users.noreply.github.com>
2018-01-21 03:54:08 -0300

    Add files via upload

---
+++ sistema_feed.py
@@ -0,0 +1,29 @@
+class Perfil:
+    def __init__(self):
+        self.bibliografia = str
+        self.amigos = list
+        self.notificacao = list
+
+    def menu_feed(self):
+        print('\n\n')
+        print('Bem vindo')
+        print('1 - Editar Bibliografia')
+        print('2 - Abrir bate papo')
+        print('3 - Ver perfil')
+        print('x - Sair')
+        opcao = input('Digite a opção: ')
+
+        if opcao == '1':
+            print('Digite algo sobre você para que os outros possam lhe conhecer melhor!')
+            self.bibliografia = input('Digite: ')
+        if opcao == 2:
+            pass#aqui vem menu do bate papo
+
+        if opcao == 3:
+            print(self.bibliografia)
+            c = 0
+            for x in self.amigos:
+                c+=1
+            print('amigos =',c)
+
+        return(opcao)
