07eae7cd89a9ec8b4179702ee1c1c493697d8c19
eliabepaz <32855849+eliabepaz@users.noreply.github.com>
2018-01-27 21:48:58 -0300

    bate_papo
    menu adicionado

--- batepapo.py
+++ batepapo.py
@@ -3,8 +3,23 @@ sis = Sistem
 class Batepapo:
     def __init__(self):
         self.conversas = list
+    def menu(self):
+        print('\n\n')
+        print('Bate papo')
+        print('1 - Nova conversa')
+        print('2 - Ver todas as conversas')
+        print('3 - Nova menssagem')
+        print('x - Sair')
+        opcao = input('Digite a opção: ')
 
-
+        if opcao == '1':
+           mensagem.nova_conversa(None)
+        if opcao == '2':
+            for x in self.conversas:
+                print(x)
+        if opcao == '3':
+            mensagem.nova_msg(None)
+        return (opcao)
 class Conversa:
     def __init__(self):
         self.menssagens = list
