a0b67167c2366beaeeef8aa619b5a93241456548
eliabepaz <32855849+eliabepaz@users.noreply.github.com>
2018-01-28 23:07:57 -0300

    Update
    classe bd inserida como armazenadora de contas;

--- acounts.py
+++ acounts.py
@@ -1,10 +1,20 @@
-class Dados:
+class Conta:
     def __init__(self):
         self.nome = str
         self.idade = int
         self.telefone = str
         self.endereco = str
+
 class Loguin:
     def __init__(self):
         self.email = str
-        self.senha = str
+        self.senha =str
+
+class Bd:
+    def __init__(self):
+        self.contas = []
+        self.loguin = []
+
+    def insert_user(self,user,log):
+        self.contas.append(user)
+        self.loguin.append(log)
