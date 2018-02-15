89e65cbad95001adee7327f03b6ae69f6461d61e
eliabepaz <32855849+eliabepaz@users.noreply.github.com>
2018-02-15 09:19:19 -0300

    Update sistema.py

--- sistema.py
+++ sistema.py
@@ -1,47 +1,34 @@
-from sistema import Sistem
-sis = Sistem()
-class Batepapo:
+from acounts import Bd
+from acounts import Conta
+from acounts import Loguin
+from feed import Perfil
+banco = Bd()
+log = Loguin()
+perfil = Perfil()
+class Sistem:
     def __init__(self):
-        self.conversas = list
+        pass
+
     def menu(self):
-        print('\n\n')
-        print('Bate papo')
-        print('1 - Nova conversa')
-        print('2 - Ver todas as conversas')
-        print('3 - Nova menssagem')
+        print('1 - Cadastrar conta')
+        print('2 - Loguin')
         print('x - Sair')
         opcao = input('Digite a opção: ')
 
         if opcao == '1':
-           mensagem.nova_conversa(None)
+            usuario = Conta
+            usuario.nome = input('Digite seu nome: ')
+            usuario.idade = input('Digite sua idade: ')
+            usuario.telefone = input('Digite seu telefone: ')
+            usuario.endereco = input('Digite seu endereço: ')
+            log.email = input('Digite seu email: ')
+            log.senha = input('Digite sua senha: ')
+            banco.insert_user(usuario,log)
         if opcao == '2':
-            for x in self.conversas():
-                print(x)
-        if opcao == '3':
-            mensagem.nova_msg(None)
-        return (opcao)
-class Conversa:
-    def __init__(self):
-        self.menssagens = list
-
-class mensagem:
-    def __init__(self):
-        self.texto = str
-
-    def nova_conversa(self):
-        destino = input('Digite o nome do destinatario: ')
-        for x in sis.contas:
-            if x.nome == destino:
-                txt = self.texto
-                new_con = Conversa()
-                new_con.menssagens.append(txt)
-    def nova_msg(self):
-        destino = input('Digite o nome do outro prosador: ')
-        new_ms = mensagem()
-        con = Batepapo()
-        for x in con.conversas:
-            if x.nome == destino:
-                for y in x:
-                    print(y)
-                new_ms.texto = input('Digite a menssagem: ')
-                x.append(new_ms.texto)
+            email = input('Digite seu email: ')
+            senha = input('Digite sua senha: ')
+            if email in log.email and senha in log.senha:
+                opc = ''
+                while opc != 'x':
+                    opc = perfil.menu_feed(email, senha)
+        return(opcao)
